3b953ac47ef0cfe5ce9c0de2dcf0e948c56504d7
bkarl <stereoklinke@googlemail.com>
2020-09-27 18:50:57 +0200

    seperated FragmentGraph and BarChart drawing logic

---
+++ app/src/main/java/com/bk/bicycletracker/BarChartFiller.java
@@ -0,0 +1,100 @@
+package com.bk.bicycletracker;
+
+import com.bk.bicycletracker.DatabaseOperations.DistanceCalculator;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.AxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+public class BarChartFiller {
+    private BarChart barchart;
+
+    public BarChartFiller(BarChart barchart) {
+        this.barchart = barchart;
+    }
+
+    public float fillWeeklyData(DistanceCalculator distanceCalculator) {
+        Calendar cal = Calendar.getInstance();
+        List<BarEntry> entries = new ArrayList<BarEntry>();
+        float totalWeek = 0;
+        final String[] days = new String[7];
+        cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY); //TODO: set according to locale
+
+        for (int i = 0; i < 7; i++)
+        {
+            float trackDay = distanceCalculator.getDistanceForDay(cal);
+            days[i] = cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.ENGLISH);
+            entries.add(new BarEntry(i, trackDay));
+            totalWeek += trackDay;
+            cal.add(Calendar.DAY_OF_YEAR, 1);
+        }
+
+        AxisValueFormatter formatter = new AxisValueFormatter() {
+
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return days[(int) value];
+            }
+
+            // we don't draw numbers, so no decimal digits needed
+            @Override
+            public int getDecimalDigits() {  return 0; }
+        };
+
+
+        BarDataSet dataSet = new BarDataSet(entries, "");
+        dataSet.setLabel("");
+
+
+        BarData bd = new BarData(dataSet);
+        bd.setValueFormatter(new BarChartFiller.KMValueFormater());
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        barchart.getXAxis().setDrawGridLines(false);
+        barchart.getXAxis().setDrawAxisLine(false);
+        barchart.getXAxis().setDrawLabels(true);
+        barchart.getXAxis().setValueFormatter(formatter);
+        barchart.getXAxis().setGranularity(1);
+        barchart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        barchart.setFitBars(true);
+
+        barchart.getAxisLeft().setDrawGridLines(false);
+        barchart.getAxisLeft().setDrawAxisLine(false);
+        barchart.getAxisLeft().setDrawLabels(false);
+        barchart.getAxisLeft().setDrawZeroLine(true);
+
+        barchart.getAxisRight().setEnabled(false);
+
+        barchart.setTouchEnabled(false);
+        barchart.setDescription("");
+        barchart.setData(bd);
+        barchart.invalidate();
+        return totalWeek;
+    }
+
+    public class KMValueFormater implements ValueFormatter {
+
+
+        public KMValueFormater() {
+
+        }
+
+        @Override
+        public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+            // write your logic here
+            return Math.round(value / 10.0)/100.0 + " km";
+        }
+    }
+}
--- app/src/main/java/com/bk/bicycletracker/FragmentGraph.java
+++ app/src/main/java/com/bk/bicycletracker/FragmentGraph.java
@@ -27,17 +27,10 @@ import java.util.List;
 import java.util.Locale;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentGraph#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class FragmentGraph extends Fragment {
-
-
-    private TrackDatabaseHelper mDB;
+public class FragmentGraph extends TimeDependentFragment {
 
     private TextView txtDistanceWeek;
+    private BarChart barChart;
     public FragmentGraph() {
         // Required empty public constructor
     }
@@ -47,93 +40,30 @@ public class FragmentGraph extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    private void fillBarChart(BarChart bc)
+    private void fillBarChart()
     {
-        DistanceCalculator distanceCalculator = new DistanceCalculator(this.getContext());
-        Calendar cal = Calendar.getInstance();
-        List<BarEntry> entries = new ArrayList<BarEntry>();
-        float totalWeek = 0;
-        final String[] days = new String[7];
-        cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY); //TODO: set according to locale
-
-        for (int i = 0; i < 7; i++)
-        {
-            float trackDay = distanceCalculator.getDistanceForDay(cal);
-            days[i] = cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.ENGLISH);
-            entries.add(new BarEntry(i, trackDay));
-            totalWeek += trackDay;
-            cal.add(Calendar.DAY_OF_YEAR, 1);
-        }
-
-        AxisValueFormatter formatter = new AxisValueFormatter() {
-
-            @Override
-            public String getFormattedValue(float value, AxisBase axis) {
-                return days[(int) value];
-            }
-
-            // we don't draw numbers, so no decimal digits needed
-            @Override
-            public int getDecimalDigits() {  return 0; }
-        };
-
-
-        BarDataSet dataSet = new BarDataSet(entries, "");
-        dataSet.setLabel("");
-
-
-        BarData bd = new BarData(dataSet);
-        bd.setValueFormatter(new KMValueFormater());
-        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        bc.getXAxis().setDrawGridLines(false);
-        bc.getXAxis().setDrawAxisLine(false);
-        bc.getXAxis().setDrawLabels(true);
-        bc.getXAxis().setValueFormatter(formatter);
-        bc.getXAxis().setGranularity(1);
-        bc.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
-
-        bc.setFitBars(true);
-
-        bc.getAxisLeft().setDrawGridLines(false);
-        bc.getAxisLeft().setDrawAxisLine(false);
-        bc.getAxisLeft().setDrawLabels(false);
-        bc.getAxisLeft().setDrawZeroLine(true);
-
-        bc.getAxisRight().setEnabled(false);
-
-        bc.setTouchEnabled(false);
-        bc.setDescription("");
-        bc.setData(bd);
-        bc.invalidate();
-
+        BarChartFiller barChartFiller = new BarChartFiller(barChart);
+        float totalWeek = barChartFiller.fillWeeklyData(new DistanceCalculator(getContext()));
         txtDistanceWeek.setText(Math.round(totalWeek / 10.0)/100.0 + " km");
+    }
+
+    @Override
+    public void dateChanged(Calendar newDate) {
+        this.currentWeek = newDate;
+        updateTimeDependentStrings();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView =  inflater.inflate(R.layout.fragment_tracking_graph, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_tracks_barchart, container, false);
+        txtWeekorDate  = (TextView) rootView.findViewById(R.id.headlineDistanceWeek);
 
-        BarChart bc = (BarChart) rootView.findViewById(R.id.chart);
-        mDB = new TrackDatabaseHelper(this.getContext());
+        barChart = (BarChart) rootView.findViewById(R.id.chart);
         txtDistanceWeek = (TextView) rootView.findViewById(R.id.txtDistanceWeek);
-        fillBarChart(bc);
+        fillBarChart();
         return rootView;
-    }
-
-    public class KMValueFormater implements ValueFormatter {
-
-
-        public KMValueFormater() {
-
-        }
-
-        @Override
-        public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
-            // write your logic here
-            return Math.round(value / 10.0)/100.0 + " km";
-        }
     }
 
 }
